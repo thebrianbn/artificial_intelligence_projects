58c04a9ae5b15ccc1abf31af0b232318d42fed4f
thebrianbn <thebrianbn@gmail.com>
2018-12-04 19:27:45 -0500

    ngrams complete

--- homework_6/bbn5024.py
+++ homework_6/bbn5024.py
@@ -11,6 +11,7 @@ student_name = "Brian Nguyen"
 # Include your imports here, if any are used.
 import string
 import re
+import random
 
 ############################################################
 # Section 1: Markov Models
@@ -50,9 +51,7 @@ def ngrams(n, tokens):
     
     for token in tokens:
         all_ngrams.append((context, token))
-        if n == 1:
-            continue
-        else:
+        if n != 1:
             context = context[n-(n-1):] + (token, )
 
     return all_ngrams
@@ -60,44 +59,116 @@ def ngrams(n, tokens):
 class NgramModel(object):
 
     def __init__(self, n):
+
         self.n = n
+        self.all_ngrams = {}
+
+        # Used to increase efficiency of n = 1 models
+        self.one_gram_dict = {}
+        self.one_gram_set = set()
+        self.needs_sort = False
 
     def update(self, sentence):
-        pass
+        
+        tokens = tokenize(sentence)
+        new_ngrams = ngrams(self.n, tokens)
+
+        for context, token in new_ngrams:
+
+            if self.n == 1:
+                if token in self.one_gram_dict:
+                    self.one_gram_dict[token] += 1
+                else:
+                    self.one_gram_dict[token] = 1
+            else:
+                if context in self.all_ngrams:
+                    self.all_ngrams[context].append(token)
+                else:
+                    self.all_ngrams[context] = [token]
+
+        if self.n == 1:
+            self.needs_sort = True
 
     def prob(self, context, token):
-        pass
+
+        # If one gram, simply get probability from count of of tokens in dict
+        if self.n == 1:
+            return 1.0 * self.one_gram_dict[token] / sum(self.one_gram_dict.values())
+
+        count = 0
+        total = len(self.all_ngrams[context])
+        
+        for ngram in self.all_ngrams[context]:
+            if token == ngram:
+                count += 1
+
+        return 1.0 * count / total
 
     def random_token(self, context):
-        pass
+        
+        r = random.random()
+
+        if self.n == 1 and self.needs_sort:
+            self.one_gram_set = sorted(set(self.one_gram_dict.keys()))
+            tokens = self.one_gram_set
+            self.needs_sort = False
+        elif self.n == 1:
+            tokens = self.one_gram_set
+        else:
+            tokens = sorted(set(self.all_ngrams[context]))
+
+        prob_sum = 0
+
+        for token in tokens:
+            p = self.prob(context, token)
+            
+            if p + prob_sum > r and r > prob_sum:
+                return token
+            prob_sum += p
 
     def random_text(self, token_count):
-        pass
+
+        context = tuple("<START>" for i in range(self.n - 1))
+        random_tokens = []
+
+        for i in range(token_count):
+            r_token = self.random_token(context)
+            random_tokens.append(r_token)
+
+            if r_token == "<END>":
+                context = tuple("<START>" for i in range(self.n - 1))
+            elif self.n != 1:
+                context = context[self.n-(self.n-1):] + (r_token, )
+        
+        return " ".join(random_tokens)
 
     def perplexity(self, sentence):
-        pass
+        
+        tokens = tokenize(text)
+        prev = "<START>"
 
 def create_ngram_model(n, path):
-    pass
+
+    model = NgramModel(n)
+    with open(path, "r") as f:
+        for line in f:
+            model.update(line)
+
+    return model
+    
 
 ############################################################
 # Section 2: Feedback
 ############################################################
 
 feedback_question_1 = """
-Type your response here.
-Your response may span multiple lines.
-Do not include these instructions in your response.
+5 hours.
 """
 
 feedback_question_2 = """
-Type your response here.
-Your response may span multiple lines.
-Do not include these instructions in your response.
+No challenges.
 """
 
 feedback_question_3 = """
-Type your response here.
-Your response may span multiple lines.
-Do not include these instructions in your response.
+Creating an n-grams model from scratch was fun.
 """
--- homework_6/test_hw6.py
+++ homework_6/test_hw6.py
@@ -1,6 +1,7 @@
 from bbn5024 import *
 from nose.tools import assert_equals, assert_not_equals
 import os
+import random
 
 def test_section_1():
 
@@ -18,7 +19,74 @@ def test_section_2():
 		(('<START>', 'a'), 'b'), (('a', 'b'), 'c'), (('b', 'c'), '<END>')])
 
 
+def test_section_3():
+
+	m = NgramModel(1)
+	m.update("a b c d")
+	m.update("a b a b")
+	assert_equals(m.prob((), "a"), 0.3)
+	assert_equals(m.prob((), "c"), 0.1)
+	assert_equals(m.prob((), "<END>"), 0.2)
+
+	m = NgramModel(2)
+	m.update("a b c d")
+	m.update("a b a b")
+	assert_equals(m.prob(("<START>",), "a"), 1.0)
+	assert_equals(m.prob(("b",), "c"), 0.3333333333333333)
+	assert_equals(m.prob(("a",), "x"), 0.0)
+
+
+def test_section_4():
+
+	m = NgramModel(1)
+	m.update("a b c d")
+	m.update("a b a b")
+	random.seed(1)
+	assert_equals([m.random_token(()) for i in range(25)],
+		['<END>', 'c', 'b', 'a', 'a', 'a', 'b', 'b', '<END>', '<END>', 'c',
+		'a', 'b', '<END>', 'a', 'b', 'a', 'd', 'd', '<END>', '<END>', 'b', 'd', 'a', 'a'])
+
+	m = NgramModel(2)
+	m.update("a b c d")
+	m.update("a b a b")
+	random.seed(2)
+	assert_equals([m.random_token(("<START>",)) for i in range(6)],
+		['a', 'a', 'a', 'a', 'a', 'a'])
+	assert_equals([m.random_token(("b",)) for i in range(6)],
+		['c', '<END>', 'a', 'a', 'a', '<END>'])
+
+
+def test_section_5():
+
+	m = NgramModel(1)
+	m.update("a b c d")
+	m.update("a b a b")
+	random.seed(1)
+	assert_equals(m.random_text(13), '<END> c b a a a b b <END> <END> c a b')
+
+	m = NgramModel(2)
+	m.update("a b c d")
+	m.update("a b a b")
+	random.seed(2)
+	assert_equals(m.random_text(15), 'a b <END> a b c d <END> a b a b a b c')
+
+
+def test_section_6():
+
+	m = create_ngram_model(1, "frankenstein.txt")
+	print m.random_text(15)
+	m = create_ngram_model(2, "frankenstein.txt")
+	print m.random_text(15)
+	m = create_ngram_model(3, "frankenstein.txt")
+	print m.random_text(15)
+	m = create_ngram_model(4, "frankenstein.txt")
+	print m.random_text(15)
+
 if __name__ == "__main__":
 
 	test_section_1()
 	test_section_2()
+	test_section_3()
+	test_section_4()
+	test_section_5()
+	test_section_6()
